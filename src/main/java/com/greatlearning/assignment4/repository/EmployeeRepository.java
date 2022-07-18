768e9a6fa56e687550475cc4d77705ba80c7cd25
shreyjn <shreyjn93@gmail.com>
2022-07-19 01:50:39 +0530

    Initial Commit

--- src/main/java/com/greatlearning/assignment4/repository/EmployeeRepository.java
+++ src/main/java/com/greatlearning/assignment4/repository/EmployeeRepository.java
@@ -11,7 +11,7 @@ import com.greatlearning.assignment4.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
     /**
      *
-     * Returns a list of Employees with first given as input. If no record is found then
+     * Returns a list of Employees with first name given as input. If no record is found then
      * it returns null,
      */
      public  List<Employee> findByFirstName(String firstName);
